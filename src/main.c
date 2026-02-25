7a1bf94715d8ad0fe47dda95a75b4a127d9c6cd6
lmj0701269999 <lmj65220@gmail.com>
2026-02-25 18:13:25 +0800

    Print student name and age

--- src/main.c
+++ src/main.c
@@ -19,51 +19,23 @@ int main(void)
         "  ]\n"
         "}\n";
 
-cJSON *root=cJSON_parse(xiangmu1);
-if(root==NULL){
-printf(“failed\n”);
-return 1;
-}else{
- Printf(“success”\n);
-}
-   
-   cJSON_Delete(root);
-    return 0;
-}
-cat > src/main.c
-#include <stdio.h>
-#include <stdlib.h>
-#include "cJSON.h"
+       cJSON *root=cJSON_Parse(xiangmu1);
+       if(root==NULL){
+       printf("failed\n");
+       return 1;
+	   }else{
+              printf("success\n");
+              cJSON *student=cJSON_GetObjectItem(root,"student");
+              cJSON *name = cJSON_GetObjectItem(student, "name");
+              cJSON *age  = cJSON_GetObjectItem(student, "age");
+              if(cJSON_IsString(name)&&cJSON_IsNumber(age)){
+              	 printf("name:%s\n",name->valuestring);
+              	 printf("age:%d\n",age->valueint);
+             	
+			  }
+           }
 
-int main(void)
-{
-    const char *xiangmu1=
-    "{\n"
-        "  \"student\": {\n"
-        "    \"name\": \"lmj\",\n"
-        "    \"age\": 20\n"
-        "  },\n"
-        "  \"courses\": [\n"
-        "    { \"name\": \"math\",    \"score\": 89 },\n"
-        "    { \"name\": \"English\", \"score\": 84 },\n"
-        "    { \"name\": \"C\",       \"score\": 88 },\n"
-        "    { \"name\": \"history\",  \"score\":90 },\n"
-        "    { \"name\": \"basketball\",  \"score\":96 }\n"
-        "  ]\n"
-        "}\n";
+cJSON_Delete(root);
+return 0;
 
-cJSON *root=cJSON_parse(xiangmu1);
-if(root==NULL){
-printf(“failed\n”);
-return 1;
-}else{
- Printf(“success\n”);
 }
-   
-   cJSON_Delete(root);
-    return 0;
-}
-gcc src/main.c third_party/cJSON/cJSON.c -Ithird_party/cJSON -o build/app
-./build/app
-./build/app
-echo $?
