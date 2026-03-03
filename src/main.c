20a4a35424693608d545c9c43f431fea8bb3de89
lmj0701269999 <lmj65220@gmail.com>
2026-03-03 22:48:15 +0800

    feat: add average score and max score calculation

--- src/main.c
+++ src/main.c
@@ -40,16 +40,27 @@ int main(void){
 		}
     
        int size=cJSON_GetArraySize(courses);
+           int sum=0;
+           double averagescore=0.0;
+           int highestscore=-100;
+           char *highestcourse=NULL;
            for(int i=0;i<size;i++){
            cJSON *course=cJSON_GetArrayItem(courses,i);
            cJSON *nameofcourse=cJSON_GetObjectItem(course,"name");
            cJSON *scoreofcourse=cJSON_GetObjectItem(course,"score");
            if(cJSON_IsString(nameofcourse)&&cJSON_IsNumber(scoreofcourse)){
            	    printf("course:%s,score:%d\n",nameofcourse->valuestring,scoreofcourse->valueint);
+           	    sum=sum+scoreofcourse->valueint;
+           	    if(scoreofcourse->valueint>highestscore){
+           	    	  highestscore=scoreofcourse->valueint;
+           	    	  highestcourse=nameofcourse->valuestring;
+           	    	}
 		   }
 		}
-        
-
+		
+       averagescore=(double)sum/size;
+       printf("平均分为:%.2f\n",averagescore);
+       printf("最高分课程: %s, 分数: %d\n", highestcourse,highestscore);
 
 cJSON_Delete(root);
 return 0;
